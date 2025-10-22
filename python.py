40cbf48ee5dff580e6e967f15186cdfa4d7bc6b3
allumeghana <103098782+allumeghana@users.noreply.github.com>
2025-10-22 12:01:59 +0530

    Update python.py

--- python.py
+++ python.py
@@ -3,8 +3,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World! This is a simple Python Flask App running in Docker & Kubernetes!'
+def hello():
+    return 'Hello, world!'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run()
